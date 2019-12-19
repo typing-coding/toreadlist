e49347f596d3a92cc3ef3d00e5fa6570d5290380
typing-coding <typing.coding@gmail.com>
2019-12-19 21:35:34 +0900

    ENH : 페이지 Home html 및 css

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+import './Home.scss'
+
 
 class Home extends Component {
     render(){
         return (
-            <h1>Home Page</h1>
+            <div className="wrap">
+                <div className="main_page">
+                    <h1 className="logo">
+                        <Link to="/List">
+                            <span className="text" ref="text">TO</span>
+                            <span className="br"></span>
+                            <span className="text">
+                                <span className="do">DO</span>
+                                <span className="read">READ</span>
+                            </span>
+                            <span className="br"></span>
+                            <span className="text">LIST</span>
+                        </Link>
+                    </h1>
+                </div>
+            </div>
         )
     }
 }
