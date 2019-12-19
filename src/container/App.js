33fb3729010c7f149e6cd01d32cbbe6eb5b89fbe
typing-coding <typing.coding@gmail.com>
2019-12-20 00:06:14 +0900

    ENH : 라우트 주소 추가 - /list,/detail

--- src/container/App.js
+++ src/container/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Home from '../pages/Home/Home'
+import List from '../pages/List/List'
+import Detail from '../pages/Detail/Detail'
 
 class App extends Component {
     render(){
         return (
             <Router>
                 <Route key="/" exact path="/" component={Home} />
+                <Route key="/list" exact path="/list" component={List} />
+                <Route key="/detail" exact path="/detail" component={Detail} />
             </Router>
         );
     }
