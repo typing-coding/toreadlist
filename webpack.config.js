9dd397a6f98c3c77ed76498de1e248455328612c
typing-coding <typing.coding@gmail.com>
2019-12-19 20:01:55 +0900

    BLD : webpack babel 처리 세팅

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,17 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            include: [
+                path.resolve(__dirname,'src')
+            ],
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
     }
 }
